3b3b56b3a96eee940a79ccc7f1ddc01714777b36
soyoung.min <soyoung.min@lge.com>
2015-08-17 15:34:51 -0700

    msm: qpnp-haptic: remove workqueue to control vibrator
    Vibrator on and off used same workqueue so when the multiple
    requests were cascaded, mid request was ignored.
    That caused long enabled term and user felt irregular strong
    vibration. And direct mode doesn't need any pre warming-up or
    post cooling-down so workqueue can be removed.
    This patch removes workqueue from direct mode and controls
    on/off directly.
    
    Change-Id: I53600eea9d6586c5ee9c376fd624261be042d7de
    Signed-off-by: soyoung.min <soyoung.min@lge.com>

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -291,6 +291,7 @@ struct qpnp_hap {
 	struct qpnp_pwm_info pwm_info;
 	struct mutex lock;
 	struct mutex wf_lock;
+	struct mutex set_lock;
 	struct completion completion;
 	enum qpnp_hap_mode play_mode;
 	enum qpnp_hap_auto_res_mode auto_res_mode;
@@ -1527,6 +1528,8 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 	u8 val = 0;
 	unsigned long timeout_ns = POLL_TIME_AUTO_RES_ERR_NS;
 
+	mutex_lock(&hap->set_lock);
+
 	if (hap->play_mode == QPNP_HAP_PWM) {
 		if (on)
 			rc = pwm_enable(hap->pwm_info.pwm_dev);
@@ -1540,8 +1543,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 				qpnp_hap_auto_res_enable(hap, 0);
 
 			rc = qpnp_hap_mod_enable(hap, on);
-			if (rc < 0)
+			if (rc < 0) {
+				mutex_unlock(&hap->set_lock);
 				return rc;
+			}
 
 			rc = qpnp_hap_play(hap, on);
 
@@ -1551,8 +1556,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 					(AUTO_RES_ENABLE_TIMEOUT + 1));
 
 				rc = qpnp_hap_auto_res_enable(hap, 1);
-				if (rc < 0)
+				if (rc < 0) {
+					mutex_unlock(&hap->set_lock);
 					return rc;
+				}
 			}
 			if (hap->correct_lra_drive_freq) {
 				/*
@@ -1566,8 +1573,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 			}
 		} else {
 			rc = qpnp_hap_play(hap, on);
-			if (rc < 0)
+			if (rc < 0) {
+				mutex_unlock(&hap->set_lock);
 				return rc;
+			}
 
 			if (hap->correct_lra_drive_freq) {
 				rc = qpnp_hap_read_reg(hap, &val,
@@ -1584,6 +1593,7 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 		}
 	}
 
+	mutex_unlock(&hap->set_lock);
 	return rc;
 }
 
@@ -1592,6 +1602,7 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 {
 	struct qpnp_hap *hap = container_of(dev, struct qpnp_hap,
 					 timed_dev);
+	flush_work(&hap->work);
 
 	mutex_lock(&hap->lock);
 	hrtimer_cancel(&hap->hap_timer);
@@ -1614,13 +1625,17 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 	}
 	#ifndef VENDOR_EDIT
 	mutex_unlock(&hap->lock);
-	schedule_work(&hap->work);
 	#else //#ifdef VENDOR_EDIT
 	queue_work(vibqueue,&hap->work);
 	msleep(1);
 	mutex_unlock(&hap->lock);
 	#endif //VENDOR_EDIT
 	/* shankai 2015-07-7 modify end for optimizing the response speed of the vibrator*/
+
+	if (hap->play_mode == QPNP_HAP_DIRECT)
+		qpnp_hap_set(hap, hap->state);
+	else
+		schedule_work(&hap->work);
 }
 
 /* play pwm bytes */
@@ -2258,6 +2273,7 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 
 	mutex_init(&hap->lock);
 	mutex_init(&hap->wf_lock);
+	mutex_init(&hap->set_lock);
 
 	#ifdef VENDOR_EDIT
 	vibqueue = create_singlethread_workqueue("vibthread");
