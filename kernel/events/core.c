68dbbdb70b1515fd960c8604b5cf4de48c4d6e24
Neil Leeder <nleeder@codeaurora.org>
2015-09-24 12:14:20 -0400

    perf: stop deadlock when cpu_up fails
    When an attempt is made to free an event on a CPU which is
    no longer online, perf tries to bring the CPU online. This
    can fail, resulting in an UP_CANCELLED notifier, which
    eventually tries to acquire the ctx->mutex which is already
    being held by the cpu_up code.
    
    Removing the attempt to bring the cpu up will remove this
    deadlock, but also requires temporarily removing support of
    counting events across hotplug. This will be restored in a
    later patch.
    
    CRs-fixed: 912295
    Change-Id: Iaafa3c6688d26508857472fd5bb32139a137880e
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1407,29 +1407,6 @@ static int __perf_remove_from_context(void *info)
 	return 0;
 }
 
-#ifdef CONFIG_SMP
-static void perf_retry_remove(struct remove_event *rep)
-{
-	int up_ret;
-	struct perf_event *event = rep->event;
-	/*
-	 * CPU was offline. Bring it online so we can
-	 * gracefully exit a perf context.
-	 */
-	up_ret = cpu_up(event->cpu);
-	if (!up_ret)
-		/* Try the remove call once again. */
-		cpu_function_call(event->cpu, __perf_remove_from_context, rep);
-	else
-		pr_err("Failed to bring up CPU: %d, ret: %d\n",
-		       event->cpu, up_ret);
-}
-#else
-static void perf_retry_remove(struct remove_event *rep)
-{
-}
-#endif
-
 /*
  * Remove the event from a task's (or a CPU's) list of events.
  *
@@ -1461,8 +1438,6 @@ static void __ref perf_remove_from_context(struct perf_event *event, bool detach
 		 */
 		ret = cpu_function_call(event->cpu, __perf_remove_from_context,
 					&re);
-		if (ret == -ENXIO)
-			perf_retry_remove(&re);
 		return;
 	}
 
@@ -5720,7 +5695,6 @@ static struct pmu perf_swevent = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 #ifdef CONFIG_EVENT_TRACING
@@ -5840,7 +5814,6 @@ static struct pmu perf_tracepoint = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 static inline void perf_tp_register(void)
@@ -6068,7 +6041,6 @@ static struct pmu perf_cpu_clock = {
 	.read		= cpu_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 /*
@@ -6149,7 +6121,6 @@ static struct pmu perf_task_clock = {
 	.read		= task_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 static void perf_pmu_nop_void(struct pmu *pmu)
