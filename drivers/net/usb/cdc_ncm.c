7aab7ad80e2ea6debc74c85bad0b30996a332c9f
Bjorn Mork <bjorn@mork.no>
2016-09-07 20:17:54 +0530

    cdc_ncm: do not call usbnet_link_change from cdc_ncm_bind usbnet_link_change will call schedule_work and should be avoided if bind is failing. Otherwise we will end up with scheduled work referring to a netdev which has gone away.
    Instead of making the call conditional, we can just defer
    it to usbnet_probe, using the driver_info flag made for
    this purpose.
    
    Change-Id: I6b4f988b761a1c235107f23a071ed50796cd6148
    Fixes: 8a34b0ae8778 ("usbnet: cdc_ncm: apply usbnet_link_change")
    Reported-by: Andrey Konovalov <andreyknvl@gmail.com>
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Git-repo: http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git
    Git-commit: 4d06dd537f95683aba3651098ae288b7cbff8274
    [rsiddoji@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
    
    Conflicts:
    	drivers/net/usb/cdc_ncm.c

--- drivers/net/usb/cdc_ncm.c
+++ drivers/net/usb/cdc_ncm.c
@@ -610,8 +610,7 @@ static int cdc_ncm_bind(struct usbnet *dev, struct usb_interface *intf)
 	if (cdc_ncm_comm_intf_is_mbim(intf->cur_altsetting))
 		return -ENODEV;
 
-	/* NCM data altsetting is always 1 */
-	return cdc_ncm_bind_common(dev, intf, 1);
+	return cdc_ncm_bind_common(dev, intf, CDC_NCM_DATA_ALTSETTING_NCM);
 }
 
 static void cdc_ncm_align_tail(struct sk_buff *skb, size_t modulus, size_t remainder, size_t max)
