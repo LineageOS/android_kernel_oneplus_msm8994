d33081d812e5d6e1f780e953cc131a003029b0de
Thierry Strudel <tstrudel@google.com>
2017-02-20 13:22:40 +0530

    ANDROID: cpu: send KOBJ_ONLINE event when enabling cpus
    In case some sysfs nodes needs to be labeled with a different label than
    sysfs then user needs to be notified when a core is brought back online.
    
    Change-Id: I1ad7445fb3067f063eeb2a976efe894aaae705a6
    Signed-off-by: Thierry Strudel <tstrudel@google.com>
    Bug: 29359497
    Git-repo: https://android.googlesource.com/kernel/msm.git
    Git-commit: 872e24621a63b75b3b644f6369b04bfb092264b1
    CRs-FIXED: 2002167
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -546,6 +546,7 @@ void __weak arch_enable_nonboot_cpus_end(void)
 void __ref enable_nonboot_cpus(void)
 {
 	int cpu, error;
+	struct device *cpu_device;
 
 	/* Allow everyone to use the CPU hotplug again */
 	cpu_maps_update_begin();
@@ -560,6 +561,12 @@ void __ref enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		if (!error) {
 			printk(KERN_INFO "CPU%d is up\n", cpu);
+			cpu_device = get_cpu_device(cpu);
+			if (!cpu_device)
+				printk(KERN_ERR "%s: failed to get cpu%d device\n",
+				       __func__, cpu);
+			else
+				kobject_uevent(&cpu_device->kobj, KOBJ_ONLINE);
 			continue;
 		}
 		printk(KERN_WARNING "Error taking CPU%d up: %d\n", cpu, error);
