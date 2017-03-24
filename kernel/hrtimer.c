610078f93eab54950452f976724345b117b2b59e
qiuchangping <aaron.qiu@oneplus.net>
2017-03-24 18:48:09 +0800

    Synchronize codes for OxygenOS 3.5.8 The changes list: 1.Recording 4K video capture process caused by the phone into the crash 2.Fix some stability issue and crash issue 3.add some secure patch
    Change-Id: I1d03e03d42b95c14ebe7178da659e5dabef1cc68
    
    Conflicts:
    	arch/arm64/configs/msm8994_defconfig
    	drivers/misc/qcom/qdsp6v2/audio_hwacc_effects.c
    	drivers/soc/qcom/subsystem_restart.c
    	include/linux/uidgid.h
    	sound/core/timer.c
    	sound/soc/msm/qdsp6v2/msm-audio-effects-q6-v2.c
    	sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
    	sound/soc/msm/qdsp6v2/msm-ds2-dap-config.c

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -1620,6 +1620,11 @@ SYSCALL_DEFINE2(nanosleep, struct timespec __user *, rqtp,
 		struct timespec __user *, rmtp)
 {
 	struct timespec tu;
+#ifdef VENDOR_EDIT
+	//xianglin add for [RAINS-3040]
+	struct timespec ctu;
+	struct task_struct *g_leader = current->group_leader;
+#endif
 
 	if (copy_from_user(&tu, rqtp, sizeof(tu)))
 		return -EFAULT;
@@ -1627,6 +1632,15 @@ SYSCALL_DEFINE2(nanosleep, struct timespec __user *, rqtp,
 	if (!timespec_valid(&tu))
 		return -EINVAL;
 
+#ifdef VENDOR_EDIT
+	getnstimeofday(&ctu);
+	ctu = timespec_add(ctu, tu);
+	if (timespec_compare(&ctu, &g_leader->ttu) > 0) {
+		g_leader->ttu.tv_sec = ctu.tv_sec;
+		g_leader->ttu.tv_nsec = ctu.tv_nsec;
+	}
+#endif
+ 
 	return hrtimer_nanosleep(&tu, rmtp, HRTIMER_MODE_REL, CLOCK_MONOTONIC);
 }
 
