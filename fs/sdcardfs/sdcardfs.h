b0408392f146af0a6d72453ecdec8e2df7e66d6d
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-05-08 19:42:22 +0200

    Revert "sdcardfs: remove effectless config option"
    This reverts commit 7ce40ba8b7119493d9607a9afdafb6e3783b27b1.

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -123,6 +123,11 @@ typedef enum {
     PERM_ANDROID_MEDIA,
 } perm_t;
 
+typedef enum {
+	LOWER_FS_EXT4,
+	LOWER_FS_FAT,
+} lower_fs_t;
+
 struct sdcardfs_sb_info;
 struct sdcardfs_mount_options;
 
@@ -186,6 +191,7 @@ struct sdcardfs_mount_options {
 	gid_t fs_low_gid;
 	userid_t fs_user_id;
 	gid_t gid;
+	lower_fs_t lower_fs;
 	mode_t mask;
 	bool multiuser;
 	unsigned int reserved_mb;
