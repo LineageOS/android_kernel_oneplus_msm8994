c40b61ace84f8715fd388089cebbfaf7e7652c59
Simon Shields <simon@lineageos.org>
2017-08-29 01:48:37 +1000

    usb: gadget: f_mtp: fix kernel panic on unplug/replug
    Change-Id: I50640419725071f1a7a29389a272831a1a1c3c32

--- drivers/usb/gadget/f_mtp.c
+++ drivers/usb/gadget/f_mtp.c
@@ -687,6 +687,9 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 
 	DBG(cdev, "mtp_read(%zu)\n", count);
 
+	if (dev == NULL || dev->ep_out == NULL)
+		return -ENODEV;
+
 	len = ALIGN(count, dev->ep_out->maxpacket);
 
 	if (len > mtp_rx_req_len)
